8bd970c33a1b6858973a6537928e813aee5cfc6e
Thao Dinh <ardtimeit@gmail.com>
2021-07-25 18:59:24 +0700

    add section2

---
+++ rbe/section5/53_inference.rs
@@ -0,0 +1,17 @@
+fn main() {
+    // Because of the annotation, the compiler knows that `elem` has type u8.
+    let elem = 5u8;
+
+    // Create an empty vector (a growable array).
+    let mut vec = Vec::new();
+    // let vec = Vec::<u8>::new(); // if comment out line 12, vec needs to declare like this, giving `vec` the explicit type `Vec<T>`, where the type parameter `T` is specified 
+    // At this point the compiler doesn't know the exact type of `vec`, it
+    // just knows that it's a vector of something (`Vec<_>`).
+
+    // Insert `elem` in the vector.
+    vec.push(elem);
+    // Aha! Now the compiler knows that `vec` is a vector of `u8`s (`Vec<u8>`)
+    // TODO ^ Try commenting out the `vec.push(elem)` line
+
+    println!("{:?}", vec);
+}
